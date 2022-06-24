0818f1b5b1c00befceaa9c3d60e23c64efed4cf6
hello-liang <liangshuaibing@outlook.com>
2022-06-24 16:07:18 +0800

    model_camrea

--- mutiprocessing_collect_data_3.py
+++ mutiprocessing_collect_data_3.py
@@ -146,7 +146,7 @@ def collect_data():
                 prediction = detections
                 result_max = str(prediction[0]+1)
 
-            test_frames = []
+            test_frames = test_frames[(len(test_frames)-num_frame_analysis):len(test_frames)]
 
         cv2.putText(image, "begin", (100, 100), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 0), 2, 0)  # (col,row) begin
         cv2.imshow('MediaPipe Hands', image)
--- mutiprocessing_predict_and_collect_data_3.py
+++ mutiprocessing_predict_and_collect_data_3.py
@@ -150,7 +150,8 @@ def predict_collect():
                 prediction = detections
                 result_max = str(prediction[0]+1)
 
-            test_frames = []
+
+            test_frames = test_frames[(len(test_frames)-num_frame_analysis):len(test_frames)]
         cv2.putText(image, result_max, (100, 100), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 0), 2, 0)  # (col,row) begin
         cv2.imshow('MediaPipe Hands', image)
         if cv2.waitKey(1) & 0xFF == ord('q'):
